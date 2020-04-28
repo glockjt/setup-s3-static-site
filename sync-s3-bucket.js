edc2c0e8c13eb5de115108b2aea8637d71b8fd22
Jonathan Glock <jonathan.glock@hotelengine.com>
2020-04-28 12:49:59 -0600

    Test new output.

--- sync-s3-bucket.js
+++ sync-s3-bucket.js
@@ -4,6 +4,6 @@ module.exports = function syncS3Bucket(srcDir, bucketName) {
   const stdout = execSync(
     `AWS_ACCESS_KEY_ID=${process.env.AWS_ACCESS_KEY_ID} AWS_SECRET_ACCESS_KEY=${process.env.AWS_SECRET_ACCESS_KEY} aws s3 sync ${srcDir}/ s3://${bucketName}/`
   );
-  console.log(stdout);
+  console.log(stdout.toString());
   return;
 };
