a25b440fb0c9eacf382337fa4594d4d1b4eb2cad
Jonathan Glock <jonathan.glock@hotelengine.com>
2020-04-30 10:54:42 -0600

    Updated Workflows (#3)
    * Updated wokrflows
    
    * Add try catch
    
    * Const to let.
    
    * Debug.
    
    * Add await.
    
    * Fix async.
    
    * Test download artifact.
    
    * Fixed artifact dir.
    
    * Check delete.
    
    * Test delete.

--- index.js
+++ index.js
@@ -13,7 +13,9 @@ async function run() {
     // console.log(JSON.stringify(payload, null, 2));
     // console.log(`prLabel: `, prLabel.replace(":", "-"));
     const bucketName = prLabel.replace(":", "-");
-    const bucketExists = s3BucketExists(bucketName);
+    const bucketExists = await s3BucketExists(bucketName);
+
+    console.log(`bucketExists: `, bucketExists);
 
     if (!bucketExists) {
       await createS3Bucket(bucketName);
